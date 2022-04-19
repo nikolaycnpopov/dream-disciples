c1e5f4fc7870a4db407708d220a84d4d1c763f75
Nikolay Popov <nikolay.cn.popov@gmail.com>
2022-04-19 23:54:06 +0300

    Cleanup.

---
+++ backend/src/main/kotlin/com/dreamdisciples/backend/ecs/ECS.kt
@@ -0,0 +1,9 @@
+package com.dreamdisciples.backend.ecs
+
+class Grid
+
+class Battle(grid: Grid, attackingBattleSquad: BattleSquad, defendingBattleSquad: BattleSquad)
+
+class BattleSquad(battleUnits: List<BattleUnit>)
+
+class BattleUnit()
